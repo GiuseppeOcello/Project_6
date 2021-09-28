b0545967ef66881e7430ee35b0ad86c9063c9d2c
GiuseppeOcello <ocello.giuseppe@gmail.com>
2021-09-28 21:38:34 +0300

    Project_6_Rev01

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 let keyboard = document.querySelector('#querty');
 let phrase = document.querySelector('phrase');
 let btn_start = document.querySelector('.btn__reset');
+const ul = document.querySelector("#phrase ul");
 
 let missed = 0;
 
@@ -19,9 +20,42 @@ const phrases = [
 ];
 
 function getRandomPhraseAsArray (arr) {
-   let rand = Math.floor(Math.random() * (arr.lenght - 0) ) + 0;
-   
+   let rand = Math.floor(Math.random() * (arr.length - 0) ) + 0;
+   let splitArr = arr[rand].split();
+   return splitArr;
+}
 
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay (arr) {
+   
+   let li = document.createElement("li");
+   for (let i=0; i <=arr.lenght; i++ ) {
+      let letter = li.textContent;
+      letter = arr[i];
+      if (letter.indexOf(' ') < 0) {
+         li.className = "letter";
+      }
+      ul.appendChild("li");
+   }
 
 }
 
+addPhraseToDisplay(phraseArray);
+
+const buttons = document.querySelectorAll("#querty button");
+// for (const button of buttons) {
+//    button.addEventListener('click', () => {
+
+
+//    });
+ 
+function checkLetter (clickedButton) {
+   let letters = document.querySelectorAll(".letter");
+   for (let i = 0; letters.length; i++) {
+      if (letters[i] === 1) {
+         letters[i].className = "show";
+
+      }
+   }
+}
