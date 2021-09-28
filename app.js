dfc9e3b8f4e4eb3e5a51b29e7e33456c404cc619
GiuseppeOcello <ocello.giuseppe@gmail.com>
2021-09-29 00:04:32 +0300

    Project_6_Rev02

--- app.js
+++ app.js
@@ -1,61 +1,74 @@
 // Global Variables
 
 let keyboard = document.querySelector('#querty');
-let phrase = document.querySelector('phrase');
+let phrase = document.querySelector('#phrase ul');
 let btn_start = document.querySelector('.btn__reset');
-const ul = document.querySelector("#phrase ul");
+let letters = document.querySelectorAll('.letter');
 
+// Variable used to keep track of the points of the player
 let missed = 0;
 
+// Hides the overlay when the start button is clicked
 btn_start.addEventListener('click', ()=> {
    btn_start.parentElement.style.display = 'none';
 });
 
+// Phrases
 const phrases = [
    'Italy has the shape of a boot',
    'The moon is gray',
    'The tallest mountain is Everest',
    'The biggest ocean is the Pacific Ocean',
-   'The sun is bigs'
+   'The sun is big'
 ];
 
+// Randomly picks on phrase from the phrases array
 function getRandomPhraseAsArray (arr) {
-   let rand = Math.floor(Math.random() * (arr.length - 0) ) + 0;
-   let splitArr = arr[rand].split();
+   let rand = Math.floor(Math.random() * (arr.length - 0) ) + 0; 
+   let splitArr = arr[rand].split("");
    return splitArr;
 }
-
 const phraseArray = getRandomPhraseAsArray(phrases);
+
 
 function addPhraseToDisplay (arr) {
    
    let li = document.createElement("li");
+   let letter = li.textContent;
    for (let i=0; i <=arr.lenght; i++ ) {
-      let letter = li.textContent;
+      
       letter = arr[i];
       if (letter.indexOf(' ') < 0) {
          li.className = "letter";
       }
-      ul.appendChild("li");
+      phrase.appendChild(li);
    }
 
 }
 
 addPhraseToDisplay(phraseArray);
 
+console.log(phrase);
+
 const buttons = document.querySelectorAll("#querty button");
 // for (const button of buttons) {
-//    button.addEventListener('click', () => {
+qwerty.addEventListener('click', (e) => {
+let clickedButton = e.target;
+clickedButton.className = 'chosen';
+clickedButton.disabled = 'true';
+
+console.log(clickedButton.className);
+
+checkLetter(clickedButton);
 
 
-//    });
- 
 function checkLetter (clickedButton) {
-   let letters = document.querySelectorAll(".letter");
+   
    for (let i = 0; letters.length; i++) {
-      if (letters[i] === 1) {
+      if (letters[i].textContent === clickedButton) {
          letters[i].className = "show";
 
       }
    }
 }
+});
