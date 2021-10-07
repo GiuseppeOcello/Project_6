a439be52042e712e306ebb16c8613ad40e20196a
GiuseppeOcello <ocello.giuseppe@gmail.com>
2021-10-08 00:21:14 +0300

    Project_6_Rev_5

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
 let keyboard = document.querySelector('#querty');
 let phrase = document.querySelector('#phrase ul');
 let btn_start = document.querySelector('.btn__reset');
+let scoreBoard = document.querySelectorAll('.tries img');
+let overlay = document.querySelector("#overlay");
 
 // Variable used to keep track of the points of the player
 let missed = 0;
 
-// Hides the overlay when the start button is clicked
+// Hides the overlay when the start button is clicked and
+// refresh the page if the button says restart
 btn_start.addEventListener('click', ()=> {
+
    btn_start.parentElement.style.display = 'none';
+
+   if (btn_start.textContent !== 'Start Game') {
+      location.reload();
+   } 
 });
 
 // Phrases
@@ -23,37 +31,32 @@ const phrases = [
 
 // Randomly picks on phrase from the phrases array
 function getRandomPhraseAsArray (arr) {
-   let rand = Math.floor(Math.random() * (arr.length - 0) ) + 0; 
-   let splitArr = arr[rand].split("");
-   console.log(splitArr);
+   let randN = Math.floor(Math.random() * (arr.length - 0) ) + 0; 
+   let splitArr = arr[randN].split("");
    return splitArr;
 }
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 
-
+// Displays blocks on the screen
 function addPhraseToDisplay (arr) {
    
    for (let i=0; i <arr.length; i++ ) {
       const li = document.createElement('li');
-      let letter = arr[i];
-      
+      let letter = arr[i]; 
+      li.textContent = letter;
+      phrase.appendChild(li); 
+
       if (letter !== " ") {
          li.className = "letter";
       }
-
-      li.textContent = letter;
-      console.log(letter);
-      phrase.appendChild(li);
+      
    }
-
 }
 
+// Calls the function to display the phrase 
 addPhraseToDisplay(phraseArray);
-
-console.log(phrase);
-
-const buttons = document.querySelectorAll("#querty button");
-// for (const button of buttons) {
+let letters = document.querySelectorAll('.letter');
 
 //## Listens for Buttom clicks and compares the letters with the random phrase 
 qwerty.addEventListener('click', (e) => {
@@ -70,10 +73,9 @@ qwerty.addEventListener('click', (e) => {
    checkLetter(clickedButton);
 
    function checkLetter (clickedButton) {
-      let letters = document.querySelectorAll('.letter');
-      let scoreBoard = document.querySelectorAll('.tries img');
+      
+      
       let letterFound = 0;
-      console.log(clickedButton.textContent);
       for (let i = 0; i < letters.length; i++) {
          let letter = letters[i];
       
@@ -82,16 +84,40 @@ qwerty.addEventListener('click', (e) => {
             letterFound += 1;
          }       
       }
-      console.log(letterFound);
          if (letterFound == 0) {
             letterFound = null;
             missed += 1;
-            console.log(scoreBoard[missed-1]);
-            console.log(missed);
             scoreBoard[missed-1].src = "images/lostHeart.png";
          }
-      // return letterFound;
+   }
+   
+   checkWin(missed);
+});
+
+function checkWin (missed) {
+   let show = document.querySelectorAll('.show');
+   let title = document.querySelector(".title");
+
+   const winningText = 'Yeah, You Won!';
+   const losingText = 'Oops, You Lost. Try again.';
+
+   function output (classN, overlayText) {
+      overlay.classList.add(classN);
+      title.innerHTML = overlayText;
+      btn_start.textContent = 'Restart';
+      overlay.style.display = 'flex';
    }
 
-   
-});
+   if (show.length === letters.length) {
+
+      output('win',winningText);
+
+   } else if (missed > 4) {
+
+      output('lose',losingText);
+      let p = document.createElement('p');
+      p.textContent = ` The Phrase was: "${phraseArray.join("")}"`;
+      overlay.appendChild(p);
+
+   }
+}
